9d778452d95acc247b253f3387cf1d806e3a9614
Bartosz Koza <bartoszkoza09@gmail.com>
2023-08-08 17:18:02 +0200

    api working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,41 +11,56 @@ import { stringify } from 'json5'
 
 
 
-export default function Home() {
-
+export default function  Home() {
+  const [message, setMessage] = useState('');
   const [data, setData] = useState()
 
   const callAPI = async () => {
+  const url = 'https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ebf8fe15a0msh9347bf8a12ae3a9p114e9ajsn431be86ffa31',
+		'X-RapidAPI-Host': 'trivia-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+  setData(result)
+} catch (error) {
+	console.error(error);
+}
+  };
+  
+  const handleChange = event => {
+    setMessage(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
   
 
-    const axios = require('axios');
-  
-  const options = {
-    method: 'GET',
-    url: 'https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia',
-    headers: {
-      'X-RapidAPI-Key': 'ebf8fe15a0msh9347bf8a12ae3a9p114e9ajsn431be86ffa31',
-      'X-RapidAPI-Host': 'trivia-by-api-ninjas.p.rapidapi.com'
-    }
-  };
-  
-  try {
-    const response = await axios.request(options);
-    setData(response.data)
-    console.log(response.data);
-  } 
-  catch (error) {
-    console.error(error);
-  }
-  };
 
   return (
     <div className='main'>
       <div className='box '>
-        <h1 className='question'>{stringify(data)}</h1>
-        <button onClick={callAPI}>Make API call</button>
-        <input placeholder='odpowiedź' className='input'></input>
+      <button onClick={callAPI}>Next question</button>
+        {data &&  
+              data.map(({ question, category }) => {
+                return(
+                  <>
+                  <h1>category: {category}</h1>
+                  <h1>{question} ?</h1>
+                  <input placeholder='odpowiedź' className='input' onChange={handleChange} value={message} />
+                  <h1>{message}</h1>
+                  </>
+                )
+              })}
       </div>
     </div>
   )
+  
 }
