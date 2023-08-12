ac87294e53c97b7477e11c21ccc2a05bb41405ac
Bartosz Koza <bartoszkoza09@gmail.com>
2023-08-12 16:17:27 +0200

    answer system half done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 'use client'
 
-import Image from 'next/image'
 import './styles.css'
-import axios from 'axios'
 import React from 'react'
-import { useState } from 'react'
-import { stringify } from 'json5'
-
-
+import { useState, useEffect } from 'react'
+import { Button } from '@mui/material'
 
 
 
 export default function  Home() {
   const [message, setMessage] = useState('');
   const [data, setData] = useState()
+  const [ans, setAns] = useState()
+  const [cat, setCat] = useState()
+  const [ques, setQues] = useState()
+  const [goodans, setGoodans] = useState(0)
+  const [badans, setBadans] = useState(0)
+  const [msg, setMsg] = useState("")
+  
 
   const callAPI = async () => {
   const url = 'https://trivia-by-api-ninjas.p.rapidapi.com/v1/trivia';
@@ -35,12 +38,14 @@ try {
 }
   };
   
+
+
   const handleChange = event => {
     setMessage(event.target.value);
-
-    console.log('value is:', event.target.value);
+    console.log('value is:', event.target.value, 'msg:', msg);
   };
 
+  
   
 
 
@@ -49,14 +54,34 @@ try {
       <div className='box '>
       <button onClick={callAPI}>Next question</button>
         {data &&  
-              data.map(({ question, category }) => {
+              data.map(({ question, category, answer }) => {  
+                
+                
+                const CheckAnswer = () =>{
+        
+                  if(message != answer){
+                    setGoodans(goodans + 1)
+                    return(
+                      <h1>dobrze !!!</h1>
+                      
+                    )
+                  } if(message == answer){
+                    <h1>siema</h1>
+                    setBadans(badans + 1)
+                  }
+                }   
+
+
                 return(
-                  <>
+                  <div key={data}>
                   <h1>category: {category}</h1>
                   <h1>{question} ?</h1>
                   <input placeholder='odpowiedź' className='input' onChange={handleChange} value={message} />
+                  <Button variant="contained" onClick={CheckAnswer}>Contained</Button>
+                  <h1>good answers: {goodans}</h1>
+                  <h1>bad answers: {badans}</h1>
                   <h1>{message}</h1>
-                  </>
+                  </div>
                 )
               })}
       </div>
